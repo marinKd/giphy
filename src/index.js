bbfd2df3aa5282b42fff0280d4b0cda678d0c3b6
Marin <52679532+marinKd@users.noreply.github.com>
2021-01-17 23:20:20 -0500

    trending gifs will appear after any key input and delete.

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import SearchField from './components/SearchField'
 import request from 'superagent';
 import GifList from './components/GifList'
+
 //7X5CFD0oV8ylPWfiLtsUkcFNuBy50raw api key
 //https://api.giphy.com/v1/gifs/trending?api_key=7X5CFD0oV8ylPWfiLtsUkcFNuBy50raw&limit=5&rating=g
 //https://api.giphy.com/v1/gifs/search?api_key=7X5CFD0oV8ylPWfiLtsUkcFNuBy50raw&q=KEYWORD___HERE&limit=5&offset=0&rating=g&lang=en
@@ -20,12 +21,19 @@ class App extends React.Component{
 
 
   handleTermChange(term){
-    const url = `http://api.giphy.com/v1/gifs/search?q=${term}&api_key=7X5CFD0oV8ylPWfiLtsUkcFNuBy50raw`;
-    request.get(url, (err, res) => {
-      this.setState({gifs: res.body.data})
-    });
+    if(term==''){
+      const url = 'http://api.giphy.com/v1/gifs/trending?api_key=7X5CFD0oV8ylPWfiLtsUkcFNuBy50raw&limit=5';
+      request.get(url, (err, res) => {
+        this.setState({ gifs: res.body.data })
+      });
+    }
+    else{
+      const url = `http://api.giphy.com/v1/gifs/search?q=${term}&api_key=7X5CFD0oV8ylPWfiLtsUkcFNuBy50raw&limit=5`;
+      request.get(url, (err, res) => {
+        this.setState({ gifs: res.body.data })
+      });
+    }
   }
-
   render(){
     return(
       <div>
